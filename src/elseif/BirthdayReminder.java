17392db18b3f055432a9dc809ff63ceb9803dad6
100297 <league@WTS19.attlocal.net>
2018-05-20 15:59:35 -0700

    CrazyCatLazy, BirthdayReminder

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -9,23 +9,35 @@ import javax.swing.JOptionPane;
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+
 		// 1. correct the birthdays for your family below
 		String momsBirthday = "January 24th";
 		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String myBirthday = "September 30th";
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String birthday = JOptionPane.showInputDialog("Which birthday do you want?");
 		// 3. Print out what the user typed
-		
+		System.out.println(birthday);
 		// 4. if user asked for "mom"
-			//print mom's birthday
-		// 5. if user asked for "dad"
-			// print dad's birthday
-		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
-	} 
+		// print mom's birthday
+		if (birthday.equals("mom")) {
+			JOptionPane.showMessageDialog(null, "" + momsBirthday);
+		}
+	// 5. if user asked for "dad"
+	// print dad's birthday
+		else if (birthday.equals("dad")) {
+	JOptionPane.showMessageDialog(null, dadsBirthday);
+	}
+	// 6. if user asked for your name
+	// print myBirthday
+	else if (birthday.equals("Elijah")){
+	JOptionPane.showMessageDialog(null, myBirthday);
+	}
+	// 7. otherwise print "Sorry, i don't remember that person's birthday!"
+else { 
+	JOptionPane.showMessageDialog(null, "Sorry, i dont remember that persons birthday.");
+}
+}
 }
